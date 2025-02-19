f5dee392b8e1ba531358ba25414e0c5ccd138cdf
jaydanflo <jaydanf2@gmail.com>
2025-02-19 10:45:45 -0500

    Lab4

--- src/main/java/com/example/loginassignment/SplashScreenApplication.java
+++ src/main/java/com/example/loginassignment/SplashScreenApplication.java
@@ -3,19 +3,22 @@ package com.example.loginassignment;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class SplashScreenApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(SplashScreenApplication.class.getResource("splash_screen.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
+
+
 
     public static void main(String[] args) {
         launch();
---
+++ src/main/java/com/example/loginassignment/SplashScreenController.java
@@ -0,0 +1,18 @@
+package com.example.loginassignment;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class SplashScreenController {
+    @FXML
+    private ImageView image1;
+
+    @FXML
+    private Button helloButton;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        helloButton.setOnAction(e -> );
+    }
+}
